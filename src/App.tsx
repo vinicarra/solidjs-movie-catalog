bd758c9fcbd36719155ae87745f3b558ad4a9564
FRWK <vinicius.carra@syngenta.com>
2022-08-14 15:09:30 -0300

    update build config

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import { Routes, Route } from "@solidjs/router";
 import { CatalogPage } from "./pages/Catalog";
 const MoviePage = lazy(() => import("./pages/Movie"));
 
+const baseUrl = import.meta.env.PROD ? "/solidjs-movie-catalog" : "";
+
 const App: Component = () => {
   return (
-    <Routes base="/solidjs-movie-catalog">
+    <Routes base={baseUrl}>
       <Route path="/" component={CatalogPage} />
       <Route path="/movie" component={MoviePage} />
     </Routes>
